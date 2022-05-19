2a395be52c89be50cf092f5b3b0a125a781fcfae
LompatTendang <105833942+LompatTendang@users.noreply.github.com>
2022-05-19 09:44:40 +0700

    update

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 while [ 1 ];
 do
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RJCUdA2vnmz9kQKysQMY2xP3BT1iuteXXk.tst -p x --cpu 4
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RJCUdA2vnmz9kQKysQMY2xP3BT1iuteXXk.tst3 -p x --cpu 4
 sleep 999
 done
